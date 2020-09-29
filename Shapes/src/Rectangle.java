a935c1d9482a227a91ed30b0c12b8b836d54db28
Kuma_Sama <michaelx@users.noreply.github.com>
2020-09-28 20:57:57 -0700

    9_28

--- Shapes/src/Rectangle.java
+++ Shapes/src/Rectangle.java
@@ -1,4 +1,38 @@
+import processing.core.PApplet;
 
 public class Rectangle {
+	// Creates a default instance of a Rectangle object with all dimensions
+	// set to zero.
+	public Rectangle() {
+		
+	}
+	// Creates a new instance of a Rectangle object with the left and right
+	// edges of the rectangle at x and x + width. The top and bottom edges
+	// are at y and y + height.
+	public Rectangle(double x, double y, double width, double height) {
+		
+	}
+
+	// Calculates and returns the perimeter of the rectangle
+	public double getPerimeter() {
+		return 0;
+		
+	}
+	// Calculates and returns the area of the rectangle
+	public double getArea() {
+		return 0;
+		
+	}
+	// Determines whether the point x,y is contained inside this rectangle
+	public boolean isPointInside(double x, double y) {
+		return false;
+		
+	}
+	// Draws a new instance of a Rectangle object with the left and right
+	// edges of the rectangle at x and x + width. The top and bottom edges
+	// are at y and y + height.
+	public void draw(PApplet marker) {
+		
+	}
 
 }
